498b842e769a0a63d91e1bae21eb92bb2762329d
Paule-Marcelle <beapaulemarcelle@gmail.com>
2026-02-02 13:22:32 +0000

    Modification modele embedding

--- chatbot_rag.py
+++ chatbot_rag.py
@@ -53,7 +53,8 @@ class ChatbotRAG:
         self.model = "llama-3.3-70b-versatile"
         
         print("📥 Chargement du modèle d'embeddings...")
-        self.embedder = SentenceTransformer('sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
+        #self.embedder = SentenceTransformer('sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
+        self.embedder = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
         
         self.documents = []
         self.embeddings = None
